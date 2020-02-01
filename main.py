3673cf611570cf2e3e865a18e45a20d62df597af
Ashirkhan857 <41200565+Ashirkhan857@users.noreply.github.com>
2020-02-01 23:14:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'HI Ashir First Application'
+     return 'HI Ashir First Application'
 
 if __name__ == '__main__':
-  app.run()
+      app.run()
